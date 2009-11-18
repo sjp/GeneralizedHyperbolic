b8c8960d4850402462fb66f021d440d6d29b6250
Simon Potter <spot023@aucklanduni.ac.nz>
2009-11-18 22:13:25 +1300

    Added matching else to length check as the error would otherwise be overidden.

--- R/gigCheckPars.R
+++ R/gigCheckPars.R
@@ -14,33 +14,33 @@ gigCheckPars <- function(Theta, ...) {
   if (length(Theta) != 3) {
     case <- "error"
     errMessage <- "Theta vector must contain 3 values"
-  }
-
-  if (chi < 0) {
-    case <- "error"
-    errMessage <- "chi must be non-negative"
-  } else if (psi < 0) {
-    case <- "error"
-    errMessage <- "psi must be non-negative"
-  } else if (isTRUE(all.equal(0, chi, ...))) {
-    if (lambda <= 0) {
+  } else {
+    if (chi < 0) {
       case <- "error"
-      errMessage <- "lambda must be positive when chi = 0"
-    } else if (isTRUE(all.equal(0, psi, ...))) {
+      errMessage <- "chi must be non-negative"
+    } else if (psi < 0) {
       case <- "error"
-      errMessage <- "psi and chi cannot both be 0"
-    } else {
-      case <- "gamma"
-    }
-  } else if (isTRUE(all.equal(0, psi, ...))) {
-    if (lambda >= 0) {
-      case <- "error"
-      errMessage <- "lambda must be negative when psi = 0"
+      errMessage <- "psi must be non-negative"
     } else if (isTRUE(all.equal(0, chi, ...))) {
-      case <- "error"
-      errMessage <- "psi and chi cannot both be 0"
-    } else {
-      case <- "invgamma"
+      if (lambda <= 0) {
+        case <- "error"
+        errMessage <- "lambda must be positive when chi = 0"
+      } else if (isTRUE(all.equal(0, psi, ...))) {
+        case <- "error"
+        errMessage <- "psi and chi cannot both be 0"
+      } else {
+        case <- "gamma"
+      }
+    } else if (isTRUE(all.equal(0, psi, ...))) {
+      if (lambda >= 0) {
+        case <- "error"
+        errMessage <- "lambda must be negative when psi = 0"
+      } else if (isTRUE(all.equal(0, chi, ...))) {
+        case <- "error"
+        errMessage <- "psi and chi cannot both be 0"
+      } else {
+        case <- "invgamma"
+      }
     }
   }
 
