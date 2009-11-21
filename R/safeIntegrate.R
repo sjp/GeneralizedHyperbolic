c546f5ee2458041435769d9c89bbfb15d1164fee
Simon Potter <spot023@aucklanduni.ac.nz>
2009-11-21 17:36:36 +1300

    Cosmetics to safeIntegrate().

--- R/safeIntegrate.R
+++ R/safeIntegrate.R
@@ -1,13 +1,17 @@
-safeIntegrate <- function (f, lower, upper, subdivisions = 100,
-                           rel.tol = .Machine$double.eps^0.25,
-                           abs.tol = rel.tol, stop.on.error = TRUE,
-                           keep.xy = FALSE, aux = NULL, ...){
+safeIntegrate <- function(f, lower, upper, subdivisions = 100,
+                          rel.tol = .Machine$double.eps^0.25,
+                          abs.tol = rel.tol, stop.on.error = TRUE,
+                          keep.xy = FALSE, aux = NULL, ...) {
+
   f <- match.fun(f)
   ff <- function(x) f(x, ...)
   equal <- all.equal(lower, upper)
-  if (is.character(equal)) equal <- FALSE
-  if (equal){
-    if (!is.finite(lower)){
+
+  if (is.character(equal))
+    equal <- FALSE
+
+  if (equal) {
+    if (!is.finite(lower)) {
       res <- NULL
       res$value <- 0
       res$abs.error <- 0
@@ -15,7 +19,7 @@ safeIntegrate <- function (f, lower, upper, subdivisions = 100,
       res$message <- "OK"
       res$call <- match.call()
       class(res) <- "integrate"
-    }else{
+    } else {
       res <- NULL
       res$value <- 0
       res$abs.error <- mean(c(ff(upper),ff(lower)))*(upper - lower)
@@ -24,10 +28,9 @@ safeIntegrate <- function (f, lower, upper, subdivisions = 100,
       res$call <- match.call()
       class(res) <- "integrate"
     }
-  }else{
+  } else {
     res <- integrate(ff, lower, upper, ...)
   }
+
   res
 }
-    
-  
