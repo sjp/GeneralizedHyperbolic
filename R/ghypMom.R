b4f2591f9eea68a2919893315c07e1b5612a44b5
Simon Potter <spot023@aucklanduni.ac.nz>
2009-11-22 17:51:04 +1300

    Cosmetics to ghypMom.R.

--- R/ghypMom.R
+++ R/ghypMom.R
@@ -2,29 +2,28 @@
 ### for the generalized hyperbolic distribution
 ### if only momType is defined, then calculate moments according to momType.
 ### if only about is defined, then calculate moment according to about.
-### if users define both momType and about(contradictive or not), then 
-### the about value would always overwrites momType. Thus, calculate moments 
+### if users define both momType and about(contradictive or not), then
+### the about value would always overwrites momType. Thus, calculate moments
 ### about "about".
 
 ghypMom <- function(order, mu = 0, delta = 1, alpha = 1, beta = 0, lambda = 1,
                     Theta = c(mu, delta, alpha, beta, lambda),
                     momType = "raw", about = 0) {
-  
-  ## check order is whole number 
-  if (!is.wholenumber(order)){
+
+  ## check order is whole number
+  if (!is.wholenumber(order))
     stop("Order must be a whole number")
-  }
-  if ((order < 0)) {
+
+  if ((order < 0))
     stop("Order must be positive")
-  } 
-  
+
   ## check momType
   momType <- as.character(momType)
   momType <- tolower(momType)
-  if (momType != "raw" & momType != "central" & momType != "mu") {
+
+  if (momType != "raw" & momType != "central" & momType != "mu")
     stop ("Unrecognised moment type")
-  } 
-  
+
   ## unpack parameters
   Theta <- as.numeric(Theta)
 
@@ -46,44 +45,48 @@ ghypMom <- function(order, mu = 0, delta = 1, alpha = 1, beta = 0, lambda = 1,
   lambda <- Theta[5]
 
   gamma <- sqrt(alpha^2 - beta^2)
-  zeta <- delta*gamma
-  
+  zeta <- delta * gamma
+
   if (order == 0) {
     mom <- 1
-  } else {         
-    ## calculate mu moments     
-    muMom <- rep (NA,order)
+  } else {
+    ## calculate mu moments
+    muMom <- rep (NA, order)
+
     for (i in 1:order) {
-      a <- momRecursion(order = i) 
-      coeff <- a$a              
-      betaPow <- a$M        
-      deltaPow <- 2*a$L
+      a <- momRecursion(order = i)
+      coeff <- a$a
+      betaPow <- a$M
+      deltaPow <- 2 * a$L
       zetaPow <- a$L
       lengthZetaPow <- length(zetaPow)
 
       ## calculate terms and sum
-      muM <- coeff*(delta^deltaPow)*(beta^betaPow)*
-        sapply(zetaPow, besselRatio, x = zeta, nu = lambda)/(zeta^zetaPow)
-      muMom[i] <- sum(muM)   
+      muM <- coeff * (delta^deltaPow) * (beta^betaPow) *
+        sapply(zetaPow, besselRatio, x = zeta, nu = lambda) / (zeta^zetaPow)
+      muMom[i] <- sum(muM)
     }
-  }  
-  
-  if (about != 0) {                    
-    mom <- momChangeAbout(order = order, oldMom = muMom, 
+  }
+
+  if (about != 0) {
+    mom <- momChangeAbout(order = order, oldMom = muMom,
                           oldAbout = mu, newAbout = about)
   } else {
-    if (momType == "mu") {
+    if (momType == "mu")
       mom = muMom[order]
-    } else if (momType == "raw") {
+
+    if (momType == "raw") {
       about <- 0
-      mom <- momChangeAbout(order = order, oldMom = muMom, 
-                            oldAbout = mu, newAbout = about)
-    } else if (momType == "central") {
-      about <- ghypMean(Theta = Theta)
-      mom <- momChangeAbout(order = order, oldMom = muMom, 
+      mom <- momChangeAbout(order = order, oldMom = muMom,
                             oldAbout = mu, newAbout = about)
     }
-  }  
+
+    if (momType == "central") {
+      about <- ghypMean(Theta = Theta)
+      mom <- momChangeAbout(order = order, oldMom = muMom,
+                            oldAbout = mu, newAbout = about)
+    }
+  }
+
   return(mom)
 }
-
