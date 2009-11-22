d7be1f288576bd92fca430b4c991b22ac7ced534
Simon Potter <spot023@aucklanduni.ac.nz>
2009-11-23 11:56:02 +1300

    Added in ghyp parameters and rearranged their order. Provided default values for all parameters. Made calls to functions with a Theta parameter explictly assign the Theta parameter.

--- R/ghypCalcRange.R
+++ R/ghypCalcRange.R
@@ -1,36 +1,41 @@
 ### Function to calculate an effective range for the distribution function
 ### or for the density function
 ### DJS 19/12/06
-ghypCalcRange <- function(Theta, tol = 10^(-5), density = TRUE, ...) 
-{
+ghypCalcRange <- function(mu = 0, delta = 1, alpha = 1, beta = 0, lambda = 1,
+                          Theta = c(mu, delta, alpha, beta, lambda),
+                          tol = 10^(-5), density = TRUE, ...) {
+
   Theta <- as.numeric(Theta)
-  if(length(Theta)==4) Theta <- c(1,Theta)
-  lambda <- Theta[1]
-  alpha <- Theta[2]
-  beta <- Theta[3]
-  delta <- Theta[4]
-  mu <- Theta[5]
+
+  if (length(Theta) == 4)
+    Theta <- c(Theta, 1)
+
+  mu <- Theta[1]
+  delta <- Theta[2]
+  alpha <- Theta[3]
+  beta <- Theta[4]
+  lambda <- Theta[5]
 
   if (density == FALSE){
     ## bounds are for distribution function
     stop("Distribution function bounds not yet implemented")
   }else{
     ## bounds are for the density function
-    mode <- ghypMode(Theta)
-    xHigh <- mode + sqrt(ghypVar(Theta))
-    while (dghyp(xHigh, Theta) > tol){
-      xHigh <- xHigh + sqrt(ghypVar(Theta))
+    mode <- ghypMode(Theta = Theta)
+    xHigh <- mode + sqrt(ghypVar(Theta = Theta))
+    while (dghyp(xHigh, Theta = Theta) > tol){
+      xHigh <- xHigh + sqrt(ghypVar(Theta = Theta))
     }
     zeroFun<-function(x){ 
-       dghyp(x, Theta) - tol 
+       dghyp(x, Theta = Theta) - tol 
      } 
     xUpper <- uniroot(zeroFun, interval = c(mode, xHigh), ...)$root
-    xLow <- mode - sqrt(ghypVar(Theta))
-    while (dghyp(xLow, Theta) > tol){
-      xLow <- xLow - sqrt(ghypVar(Theta))
+    xLow <- mode - sqrt(ghypVar(Theta = Theta))
+    while (dghyp(xLow, Theta = Theta) > tol){
+      xLow <- xLow - sqrt(ghypVar(Theta = Theta))
     }
     zeroFun<-function(x){ 
-       dghyp(x, Theta) - tol 
+       dghyp(x, Theta = Theta) - tol 
      } 
     xLower <- uniroot(zeroFun, interval = c(xLow, mode), ...)$root
     range <- c(xLower, xUpper)
