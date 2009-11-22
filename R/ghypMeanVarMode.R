8d0cdb62f56876b6c4c8869185e41577bc24c67c
Simon Potter <spot023@aucklanduni.ac.nz>
2009-11-22 17:19:47 +1300

    Changed parameters and respective order within Theta. All parameters are now checked for validity. Ensured all relevant calls using the Theta parameter were made explicit.
    Set indentation to 2 spaces in besselRatio.R.

--- R/ghypMeanVarMode.R
+++ R/ghypMeanVarMode.R
@@ -1,13 +1,26 @@
 ### Function to calculate the theoretical mean of a 
 ### generalized hyperbolic distribution given its parameters.
-ghypMean <- function(Theta){
+ghypMean <- function(mu = 0, delta = 1, alpha = 1, beta = 0, lambda = 1,
+                     Theta = c(mu, delta, alpha, beta, lambda)){
+
   Theta <- as.numeric(Theta)
-  if(length(Theta)==4) Theta <- c(1,Theta)
-  lambda <- Theta[1]
-  alpha <- Theta[2]
-  beta <- Theta[3]
-  delta <- Theta[4]
-  mu <- Theta[5]
+
+  if (length(Theta) == 4)
+    Theta <- c(Theta, 1)
+
+  ## check parameters
+  parResult <- ghypCheckPars(Theta)
+  case <- parResult$case
+  errMessage <- parResult$errMessage
+
+  if (case == "error")
+    stop(errMessage)
+
+  mu <- Theta[1]
+  delta <- Theta[2]
+  alpha <- Theta[3]
+  beta <- Theta[4]
+  lambda <- Theta[5]
 
   gamma <- sqrt(alpha^2 - beta^2)
   
@@ -16,40 +29,92 @@ ghypMean <- function(Theta){
 
 ### Function to calculate the theoretical variance of a 
 ### generalized hyperbolic distribution given its parameters.
-ghypVar <- function(Theta){
-  var <- ghypMom(2, Theta, momType = "central")
+ghypVar <- function(mu = 0, delta = 1, alpha = 1, beta = 0, lambda = 1,
+                    Theta = c(mu, delta, alpha, beta, lambda)){
+
+  Theta <- as.numeric(Theta)
+
+  if (length(Theta) == 4)
+    Theta <- c(Theta, 1)
+
+  ## check parameters
+  parResult <- ghypCheckPars(Theta)
+  case <- parResult$case
+  errMessage <- parResult$errMessage
+
+  if (case == "error")
+    stop(errMessage)
+
+  var <- ghypMom(2, Theta = Theta, momType = "central")
   return(var)
 } ## End of ghypVar()
 
 ### Function to calculate the theoretical skewness of a 
 ### generalized hyperbolic distribution given its parameters.
-ghypSkew <- function(Theta){
-  skew <- ghypMom(3, Theta, momType = "central")/(ghypVar(Theta)^(3/2))
+ghypSkew <- function(mu = 0, delta = 1, alpha = 1, beta = 0, lambda = 1,
+                     Theta = c(mu, delta, alpha, beta, lambda)){
+
+  Theta <- as.numeric(Theta)
+
+  if (length(Theta) == 4)
+    Theta <- c(Theta, 1)
+
+  ## check parameters
+  parResult <- ghypCheckPars(Theta)
+  case <- parResult$case
+  errMessage <- parResult$errMessage
+
+  if (case == "error")
+    stop(errMessage)
+
+  skew <- ghypMom(3, Theta = Theta, momType = "central")/(ghypVar(Theta = Theta)^(3/2))
   return(skew)
 } ## End of ghypSkew()
 
 ### Function to calculate the theoretical kurtosis of a 
 ### generalized hyperbolic distribution given its parameters.
-ghypKurt <- function(Theta){
-  kurt <- ghypMom(4, Theta, momType = "central")/(ghypVar(Theta)^2) - 3
+ghypKurt <- function(mu = 0, delta = 1, alpha = 1, beta = 0, lambda = 1,
+                     Theta = c(mu, delta, alpha, beta, lambda)){
+
+  Theta <- as.numeric(Theta)
+
+  if (length(Theta) == 4)
+    Theta <- c(Theta, 1)
+
+  ## check parameters
+  parResult <- ghypCheckPars(Theta)
+  case <- parResult$case
+  errMessage <- parResult$errMessage
+
+  if (case == "error")
+    stop(errMessage)
+
+  kurt <- ghypMom(4, Theta = Theta, momType = "central")/(ghypVar(Theta = Theta)^2) - 3
   return(kurt)
 } ## End of ghypKurt()
 
 ### Function to calculate the theoretical mode point of a 
 ### generalized hyperbolic distribution given its parameters.
-ghypMode <- function(Theta){
+ghypMode <- function(mu = 0, delta = 1, alpha = 1, beta = 0, lambda = 1,
+                     Theta = c(mu, delta, alpha, beta, lambda)){
+
   Theta <- as.numeric(Theta)
-  if(length(Theta)==4) Theta <- c(1,Theta)
-  lambda <- Theta[1]
-  alpha <- Theta[2]
-  beta <- Theta[3]
-  delta <- Theta[4]
-  mu <- Theta[5]
+
+  if (length(Theta) == 4)
+    Theta <- c(1, Theta)
+
+  ## check parameters
+  parResult <- ghypCheckPars(Theta)
+  case <- parResult$case
+  errMessage <- parResult$errMessage
+
+  if (case == "error")
+    stop(errMessage)
 
   modeFun <- function(x){
-    log(dghyp(x, Theta))
+    log(dghyp(x, Theta = Theta))
   }
-  start <- ghypMean(Theta)
+  start <- ghypMean(Theta = Theta)
   optResult <- optim(start, modeFun,
                      control = list(fnscale = -1, maxit = 1000),
                      method = "BFGS")
