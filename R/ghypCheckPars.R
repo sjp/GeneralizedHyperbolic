03895dc234985f9a76fc3ed6938f3b78fc90ab35
Simon Potter <spot023@aucklanduni.ac.nz>
2009-11-29 17:25:30 +1300

    Created documentation for ghypCheckPars.R. Corrected indentation for ghypCheckPars().

--- R/ghypCheckPars.R
+++ R/ghypCheckPars.R
@@ -1,7 +1,7 @@
 ### Check for boundary and error cases
 ### Return case normal, or error
 ### In case of error, include appropriate message
-ghypCheckPars <- function(Theta, ...) {
+ghypCheckPars <- function(Theta) {
 
   Theta <- as.numeric(Theta)
   mu <- Theta[1]
@@ -10,61 +10,66 @@ ghypCheckPars <- function(Theta, ...) {
   beta <- Theta[4]
   lambda <- Theta[5]
 
-  case <- "normal"
+  case <- ""
   errMessage <- ""
 
   if (length(Theta) != 5) {
     case <- "error"
     errMessage <- "Theta vector must contain 5 values"
   } else {
-    if (lambda == 0) {
-      if (abs(beta) >= alpha) {
-        case <- "error"
-        errMessage <- "absolute value of beta must be less than alpha when lambda = 0"
-      }
-
-      if (delta <= 0) {
+    if (alpha < 0) {
+      case <- "error"
+      errMessage <- "alpha must not be less than zero"
+    } else {
+      if (lambda == 0) {
+        if (abs(beta) >= alpha) {
           case <- "error"
-          errMessage <- "delta must be greater than zero when lambda = 0"
+          errMessage <- "absolute value of beta must be less than alpha when lambda = 0"
+        }
+
+        if (delta <= 0) {
+            case <- "error"
+            errMessage <- "delta must be greater than zero when lambda = 0"
+        }
+
+        if (abs(beta) >= alpha & delta <= 0) {
+            case <- "error"
+            errMessage <- "absolute value of beta must be less than alpha and delta must be greater than zero when lambda = 0"
+        }
       }
 
-      if (abs(beta) >= alpha & delta <= 0) {
+      if (lambda > 0) {
+        if (abs(beta) >= alpha) {
           case <- "error"
-          errMessage <- "absolute value of beta must be less than alpha and delta must be greater than zero when lambda = 0"
-      }
-    }
+          errMessage <- "absolute value of beta must be less than alpha when lambda > 0"
+        }
 
-    if (lambda > 0) {
-      if (abs(beta) >= alpha) {
-        case <- "error"
-        errMessage <- "absolute value of beta must be less than alpha when lambda > 0"
-      }
+        if (delta < 0) {
+          case <- "error"
+          errMessage <- "delta must be less than zero when lambda > 0"
+        }
 
-      if (delta < 0) {
-        case <- "error"
-        errMessage <- "delta must be less than zero when lambda > 0"
-      }
-
-      if (abs(beta) >= alpha & delta < 0) {
-        case <- "error"
-        errMessage <- "absolute value of beta must be less than alpha and delta must be less than zero when lambda > 0"
-      }
-    }
-
-    if (lambda < 0) {
-      if (abs(beta) != alpha) {
-        case <- "error"
-        errMessage <- "absolute value of beta must be equal to alpha when lambda < 0"
+        if (abs(beta) >= alpha & delta < 0) {
+          case <- "error"
+          errMessage <- "absolute value of beta must be less than alpha and delta must be less than zero when lambda > 0"
+        }
       }
 
-      if (delta <= 0) {
-        case <- "error"
-        errMessage <- "delta must be greater than zero when lambda < 0"
-      }
+      if (lambda < 0) {
+        if (abs(beta) != alpha) {
+          case <- "error"
+          errMessage <- "absolute value of beta must be equal to alpha when lambda < 0"
+        }
 
-      if (abs(beta) != alpha & delta <= 0) {
-        case <- "error"
-        errMessage <- "absolute value of beta must be equal to alpha and delta must be greater than zero when lambda < 0"
+        if (delta <= 0) {
+          case <- "error"
+          errMessage <- "delta must be greater than zero when lambda < 0"
+        }
+
+        if (abs(beta) != alpha & delta <= 0) {
+          case <- "error"
+          errMessage <- "absolute value of beta must be equal to alpha and delta must be greater than zero when lambda < 0"
+        }
       }
     }
   }
