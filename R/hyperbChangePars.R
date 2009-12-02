0a7feb32169bb93d483a277065f4bf08f11630e9
Simon Potter <spot023@aucklanduni.ac.nz>
2009-12-03 01:55:08 +1300

    Reordering vectors from (X, X, delta, mu) to (mu, delta, X, X).

--- R/hyperbChangePars.R
+++ R/hyperbChangePars.R
@@ -61,100 +61,91 @@ hyperbChangePars <- function (from, to, Theta, noNames = FALSE) {
   if (from == 1 & to == 2) {
     alpha <- zeta * sqrt(1 + hyperbPi^2) / delta
     beta <- zeta * hyperbPi / delta
-    output <- c(alpha = alpha, beta = beta, delta = delta,
-                mu = mu)
+    output <- c(mu = mu, delta = delta, alpha = alpha, beta = beta)
   }
 
   if (from == 1 & to == 3) {
     phi <- zeta / delta * (sqrt(1 + hyperbPi^2) + hyperbPi)
     gamma <- zeta / delta * (sqrt(1 + hyperbPi^2) - hyperbPi)
-    output <- c(phi = phi, gamma = gamma, delta = delta, mu = mu)
+    output <- c(mu = mu, delta = delta, phi = phi, gamma = gamma)
   }
 
   if (from == 1 & to == 4) {
     xi <- 1 / sqrt(1 + zeta)
     chi <- hyperbPi / sqrt((1 + zeta) * (1 + hyperbPi^2))
-    output <- c(xi = xi, chi = chi, delta = delta, mu = mu)
+    output <- c(mu = mu, delta = delta, xi = xi, chi = chi)
   }
 
   if (from == 2 & to == 1) {
     hyperbPi <- beta / sqrt(alpha^2 - beta^2)
     zeta <- delta * sqrt(alpha^2 - beta^2)
-    output <- c(hyperbPi = hyperbPi, zeta = zeta, delta = delta,
-                mu = mu)
+    output <- c(mu = mu, delta = delta, hyperbPi = hyperbPi, zeta = zeta)
   }
 
   if (from == 2 & to == 3) {
     phi <- alpha + beta
     gamma <- alpha - beta
-    output <- c(phi = phi, gamma = gamma, delta = delta, mu = mu)
+    output <- c(mu = mu, delta = delta, phi = phi, gamma = gamma)
   }
 
   if (from == 2 & to == 4) {
     xi <- 1 / sqrt(1 + delta * sqrt(alpha^2 - beta^2))
     chi <- beta / (alpha * sqrt(1 + delta * sqrt(alpha^2 - beta^2)))
-    output <- c(xi = xi, chi = chi, delta = delta, mu = mu)
+    output <- c(mu = mu, delta = delta, xi = xi, chi = chi)
   }
 
   if (from == 3 & to == 1) {
     hyperbPi <- (phi - gamma) / (2 * sqrt(phi * gamma))
     zeta <- delta * sqrt(phi * gamma)
-    output <- c(hyperbPi = hyperbPi, zeta = zeta, delta = delta,
-                mu = mu)
+    output <- c(mu = mu, delta = delta, hyperbPi = hyperbPi, zeta = zeta)
   }
 
   if (from == 3 & to == 2) {
     alpha <- (phi + gamma) / 2
     beta <- (phi - gamma) / 2
-    output <- c(alpha = alpha, beta = beta, delta = delta,
-                mu = mu)
+    output <- c(mu = mu, delta = delta, alpha = alpha, beta = beta)
   }
 
   if (from == 3 & to == 4) {
     xi <- 1 / sqrt(1 + delta * sqrt(phi * gamma))
     chi <- ((phi - gamma) / (phi + gamma)) / sqrt(1 + delta * sqrt(phi * gamma))
-    output <- c(xi = xi, chi = chi, delta = delta, mu = mu)
+    output <- c(mu = mu, delta = delta, xi = xi, chi = chi)
   }
 
   if (from == 4 & to == 1) {
     hyperbPi <- chi / sqrt(xi^2 - chi^2)
     zeta <- (1 - xi^2) / xi^2
-    output <- c(hyperbPi = hyperbPi, zeta = zeta, delta = delta, mu = mu)
+    output <- c(mu = mu, delta = delta, hyperbPi = hyperbPi, zeta = zeta)
   }
 
   if (from == 4 & to == 2) {
     alpha <- (1 - xi^2) / (delta * xi * sqrt(xi^2 - chi^2))
     beta <- chi * (1 - xi^2) / (delta * xi^2 * sqrt(xi^2 - chi^2))
-    output <- c(alpha = alpha, beta = beta, delta = delta,
-                mu = mu)
+    output <- c(mu = mu, delta = delta, alpha = alpha, beta = beta)
   }
 
   if (from == 4 & to == 3) {
     phi <- (1 - xi^2) * (xi + chi) / (delta * xi^2 * sqrt(xi^2 - chi^2))
     gamma <- (1 - xi^2) * (xi - chi) / (delta * xi^2 * sqrt(xi^2 - chi^2))
-    output <- c(phi = phi, gamma = gamma, delta = delta, mu = mu)
+    output <- c(mu = mu, delta = delta, phi = phi, gamma = gamma)
   }
 
   if (from == to) {
     if (from == 1)
-      output <- c(hyperbPi = hyperbPi, zeta = zeta, delta = delta,
-                  mu = mu)
+      output <- c(mu = mu, delta = delta, hyperbPi = hyperbPi, zeta = zeta)
 
     if (from == 2)
-      output <- c(alpha = alpha, beta = beta, delta = delta,
-                  mu = mu)
+      output <- c(mu = mu, delta = delta, alpha = alpha, beta = beta)
 
-      if (from == 3)
-        output <- c(phi = phi, gamma = gamma, delta = delta,
-                    mu = mu)
+    if (from == 3)
+      output <- c(mu = mu, delta = delta, phi = phi, gamma = gamma)
 
-      if (from == 4)
-        output <- c(xi = xi, chi = chi, delta = delta,
-                    mu = mu)
+    if (from == 4)
+      output <- c(mu = mu, delta = delta, xi = xi, chi = chi)
   }
 
   if (noNames == TRUE)
-      names(output) <- NULL
+    names(output) <- NULL
 
   output
 } ## End of hyperbChangePars()
